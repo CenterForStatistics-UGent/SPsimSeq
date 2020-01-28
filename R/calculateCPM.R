ca4144f212c76b86cdaa51ffe742ab9eb48e152a
AlemuTakele <alemutak@hotmail.com>
2020-01-28 12:29:39 +0100

    V2.0.1
    Former-commit-id: 738d9fc2548c0b2b493acd7f266129127678c1d3 [formerly 3cd35709bffbcc4242df811b97308d3617aa36c9] [formerly 123ac9d246ca1e7d23c1b8871e30a5e6f28db32c] [formerly 27e34a362adb4c27ab277a856a8812d95487b260]
    Former-commit-id: 738d9fc2548c0b2b493acd7f266129127678c1d3 [formerly 3cd35709bffbcc4242df811b97308d3617aa36c9] [formerly 123ac9d246ca1e7d23c1b8871e30a5e6f28db32c]
    Former-commit-id: 738d9fc2548c0b2b493acd7f266129127678c1d3 [formerly 3cd35709bffbcc4242df811b97308d3617aa36c9]
    Former-commit-id: a83369bfc443e78da79ce4057789b4ad8e2f36e2
    Former-commit-id: 88b53461a82a48ac4e580361239dcfa4efcdda64

--- R/calculateCPM.R
+++ R/calculateCPM.R
@@ -1,42 +1,7 @@
 # Calculates counts per millions of reads
-# 
-# @param X an object with class 'SingleCellExperiment', 'data.frame', or 'matrix' 
-# typically containing gene expression data with genes in rows
-# @param const.mult a numerical constant indicatin the desired library size for 
-# all samples/cells
-# @param norm.lib.size logical value. If TRUE, normalized library size will be used to
-# calculate CPM. In particulr, TMM normalization factors will be used unless @param 
-# norm.factors is not NULL
-# @param norm.factors a numerical vector of normalization factors
-# @param logt a logical value. If TRUE, log(base=@param log_base) of the CPM 
-# will be returned
-# @param log_base an integer for the base of logarithmic transformation.
-# It will only be considered if @param log = TRUE.
-# @param prior.count a positive integer to be added to the CPM prior to log transformation.
-# It will only be considered if @param log = TRUE.
-# @param  ... further arguments passed to or from other methods.
-# 
-# @return a matrix of CPM
-# @export
-# @examples
-# \donttest{
-# dat <- make.example.data(n.gene = 10,n.sample = 5, n.group = 1, n.batch = 1)
-# cpm.dat <- calCPM(dat)
-# cpm.dat <- calCPM(dat, norm.lib.size = FALSE)
-# 
-# cpm.dat <- calCPM(dat, logt = TRUE)
-# cpm.dat <- calCPM(dat, logt = TRUE, log_base = 10)
-# 
-# cpm.dat <- calCPM(dat, logt = TRUE)
-# cpm.dat <- calCPM(dat, logt = TRUE, prior.count = 0.25)
-# }
-# 
-# @importFrom edgeR calcNormFactors
-# @importFrom SingleCellExperiment counts SingleCellExperiment
-
-calCPM <- function(X, const.mult=1e6, norm.lib.size=TRUE, norm.factors=NULL, logt=FALSE,
-                   log_base=2, prior.count=1, ...){
-  if(is(X, "SingleCellExperiment") | is(X, "matrix") | is(X, "data.frame")){ 
+calCPM <- function(X, const.mult=1e6, norm.lib.size=TRUE, norm.factors=NULL, 
+                   logt=FALSE, log_base=2, prior.count=1, ...){
+  if(is(X, "SingleCellExperiment") | is(X, "matrix") | is(X, "data.frame")){
     if(!all(dim(X)>1)){
       stop("Calculating CPM for a non matrix data!")
     }
@@ -48,9 +13,9 @@ calCPM <- function(X, const.mult=1e6, norm.lib.size=TRUE, norm.factors=NULL, log
         stop("Invalid normalization factors passed!")
       }
     }
-  }
-  else{
-    stop("Calculating CPM for a data with class not in 'SingleCellExperiment', 'data.frame', 'matrix'!")
+  }else{
+    stop("Calculating CPM for a data with class not in 
+             'SingleCellExperiment', 'data.frame', 'matrix'!")
   }
   
   if(is(X, "SingleCellExperiment")){
@@ -77,7 +42,5 @@ calCPM <- function(X, const.mult=1e6, norm.lib.size=TRUE, norm.factors=NULL, log
   }
   else{
     cpm
-  } 
+  }
 }
-
-
---
+++ inst/NEWS
@@ -0,0 +1,10 @@
+
+0.1.0
+=====
+
+-   First private GitHub document
+
+0.1.2
+=====
+
+-   Reduce build time using
