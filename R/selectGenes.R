8f9ee86efe92390232317f79b36df8a29b7d4ff5
shawinke <stijn.hawinkel@ugent.be>
2020-02-07 16:40:39 +0100

    More parsimonious returns and faster data generation
    Former-commit-id: 202bc3e647da478c998b8a195e939ab0410663f0 [formerly 0f9eb841c6848d0c463133af273ff245b5419614] [formerly abfcf5c3b77442c7af30ab538a1a9789ede4d035] [formerly e4a1b354a92b4df4f6f10c03fc708564f7fa6f32]
    Former-commit-id: 202bc3e647da478c998b8a195e939ab0410663f0 [formerly 0f9eb841c6848d0c463133af273ff245b5419614] [formerly abfcf5c3b77442c7af30ab538a1a9789ede4d035]
    Former-commit-id: 202bc3e647da478c998b8a195e939ab0410663f0 [formerly 0f9eb841c6848d0c463133af273ff245b5419614]
    Former-commit-id: 6e14818df45269a5d8c8bd1d9eb79f836679ccab
    Former-commit-id: ad9a5040204abc7bdc8586188d3ce295b18b7b13

--- R/selectGenes.R
+++ R/selectGenes.R
@@ -1,56 +1,30 @@
 # selectes genes among candidate genes
 selectGenesSim <- function(pDE, group, n.genes, null.genes0, nonnull.genes0,
-                           group.config, h, ...){
+                           group.config, ...){
   if(pDE>0 & !is.null(group) & length(group.config)>1){
     if((1-pDE)*n.genes <= length(null.genes0)){
-      #set.seed(setSimContol$seed.sample.genes[h])
       null.genes     <- sample(null.genes0, (1-pDE)*(n.genes), replace = FALSE)
     }
     else{
-      if(h==1){
-        message("Note: The number of null genes (not DE) in the source data is ", length(null.genes0)," and the number of null genes required to be included in the simulated data is ", round((1-pDE)*(n.genes)), ". Therefore, candidiate null genes are sampled with replacement. \n This message applies to the rest of the simulatons.")
-      }
-      
-      #set.seed(setSimContol$seed.sample.genes[h])
       null.genes     <- sample(null.genes0, (1-pDE)*(n.genes), replace = TRUE)
     }
     
     if(length(nonnull.genes0)>0){
       if(pDE*n.genes <= length(nonnull.genes0)){
-        #set.seed(setSimContol$seed.sample.genes[h]+1)
         nonnull.genes  <- sample(nonnull.genes0, pDE*n.genes, replace = FALSE)
       }
       else {
-        if(h==1){
-          message("Note: The number of DE genes detected in the source data is ", length(nonnull.genes0), " and the number of DE genes required to be included in the simulated data is ", round(pDE*n.genes), ". Therefore, candidiate DE genes are sampled with replacement. \n This message applies to the rest of the simulatons.")
-        }
-        #set.seed(setSimContol$seed.sample.genes[h]+1)
         nonnull.genes  <- sample(nonnull.genes0, pDE*n.genes, replace = TRUE)
       }
     }
     else{ 
       nonnull.genes <- NULL
     }
-    
-    #set.seed(NULL)
-    
     sel.genes <- c(nonnull.genes, null.genes)
-    DE.ind <- ifelse(sel.genes %in% null.genes, 0, 1)
-    names(DE.ind) <- sel.genes
-    
-  }
-  else{
-    if(n.genes <= length(null.genes0)){
-      #set.seed(setSimContol$seed.sample.genes[h])
-      sel.genes     <- sample(null.genes0, n.genes, replace = FALSE)
-    }
-    else{
-      #set.seed(setSimContol$seed.sample.genes[h])
-      sel.genes     <- sample(null.genes0, n.genes, replace = TRUE)
-    } 
-    DE.ind <- rep(0, length(sel.genes))
-    names(DE.ind) <- sel.genes
+  }  else{
+      sel.genes <- sample(null.genes0, n.genes, 
+                          replace = n.genes <= length(null.genes0))
   } 
-  
-  list(DE.ind=DE.ind, sel.genes=sel.genes)
+  names(sel.genes) = sel.genes
+  sel.genes
 }
