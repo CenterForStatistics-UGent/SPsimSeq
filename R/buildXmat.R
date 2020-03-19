130fc0b068ae153076f3b5d9be4dc81a02ecb939
shawinke <stijn.hawinkel@ugent.be>
2020-03-19 13:24:15 +0100

    Remove seed setting
    Former-commit-id: 7b45eeda7d10f05ac4fa8bd789a9196bb29184f7
    Former-commit-id: b9d1893a176298d1bebb45ce71c59cd8b2d1b98e
    Former-commit-id: abd0a9c6cdf19186609ab1f631e3af05de6fb085
    Former-commit-id: c87456d90a0e8f006517c55c328916ee38f15216
    Former-commit-id: 214f10ea800925d2cffc262b678785256d0fc799

--- R/buildXmat.R
+++ R/buildXmat.R
@@ -1,10 +1,10 @@
-#An auxialiary function to quickly construct the polyomial matrix, 
-#using Horner's rule
+#'An auxialiary function to quickly construct the polyomial matrix, 
+#'using Horner's rule
 #'
-#@param x The base 
-#@param nc the number of columns
+#'@param x The base 
+#'@param nc the number of columns
 #'
-#@return A matrix with increasing powers of x in the columns
+#'@return A matrix with increasing powers of x in the columns
 buildXmat = function(x, nc){
   x.mat <- matrix(1, nrow = length(x), ncol = nc)
   for(k in 2:nc){
