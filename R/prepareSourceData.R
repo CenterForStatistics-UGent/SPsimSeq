83e4ee3aca6e3ebfa259d0efdab8f28a6209df3d
shawinke <stijn.hawinkel@ugent.be>
2020-03-19 15:21:52 +0100

    With full man pages and without html
    Former-commit-id: 2f6b0de91499bd3d23ccbcbb551243bd9e52d20f
    Former-commit-id: 92ddf3e8b3f9fec1f7afbf356e3b193dd0a6241f
    Former-commit-id: 83a7b6b862bca2cc3412e8fb2919167628d07d7c
    Former-commit-id: 677eea1ad908dd26cd3625591af403fa9b8eb917
    Former-commit-id: 69eeedc4d4494bfd97b91388766246bc0840eb61

--- R/prepareSourceData.R
+++ R/prepareSourceData.R
@@ -1,22 +1,22 @@
-# Prepare source data for simulation.
-# 
-# @param s.data a source data (a SingleCellExperiment object)
-# @param batch a vector containg btach indicator for each sample/cell
-# @param group a vector containg group indicator for each sample/cell
-# @param cand.DE.genes a list object contating candidate predictor (DE) genes
-# @param exprmt.design a list contating simulation design configuration 
-# @param const a small constant (>0) to be added to the CPM before log transformation, to avoid  log(0).
-# default is 1e-5
-# @param simCtr (integer) seed number
-# @param  ... further arguments passed to or from other methods.
-#
-# @return a list object
-#
-# @examples
-#  # example 
-# @importFrom fitdistrplus fitdist
+#' Prepare source data for simulation.
+#' 
+#' @param s.data a source data matrix
+#' @param batch a vector containg btach indicator for each sample/cell
+#' @param group a vector containg group indicator for each sample/cell
+#' @param cand.DE.genes a list object contating candidate predictor (DE) genes
+#' @param exprmt.design a list contating simulation design configuration 
+#' @param const a small constant (>0) to be added to the CPM before log transformation, to avoid  log(0).
+#' default is 1e-5
+#' @param lfc.thrld,llStat.thrld,w see ?chooseCandGenes
+#' @param  ... further arguments passed to or from other methods.
+#'
+#' @return a list object
+#'
+#' @examples
+#'  # example 
+#' @importFrom fitdistrplus fitdist
 prepareSourceData <- function(s.data, batch, group, cand.DE.genes,  
-                              exprmt.design, simCtr, lfc.thrld, llStat.thrld,
+                              exprmt.design, const, lfc.thrld, llStat.thrld,
                               const, w, log.CPM.transform, ...){
   
   # design element
@@ -26,29 +26,16 @@ prepareSourceData <- function(s.data, batch, group, cand.DE.genes,
   
   # calculate log CPM 
   if(log.CPM.transform){
-    if(is(s.data, "SingleCellExperiment")){ 
-      cpm.data <- log(calCPM(counts(s.data))+const)
-      #LS <- colSums(counts(s.data))
-    }else if(is(s.data, "data.frame") | is(s.data, "matrix")){
       cpm.data <- log(calCPM(s.data)+const)
-      #LS <- colSums(s.data)
-    }
   }
   else{
-    if(is(s.data, "SingleCellExperiment")){ 
-      cpm.data <- counts(s.data)
-      LS <- colSums(counts(s.data))
-    }else if(is(s.data, "data.frame") | is(s.data, "matrix")){
       cpm.data <- s.data
       LS <- colSums(s.data)
-    }
   }
-  
   
   # subset batches
   if(!is.null(batch)){ 
     if(length(n.batch) < length(unique(batch))){
-      set.seed(simCtr)
       sub.batchs <- sort(sample(length(unique(batch)), length(n.batch))) 
     }else if(length(n.batch) == length(unique(batch))){
       sub.batchs <- seq_len(length(n.batch))
@@ -58,23 +45,17 @@ prepareSourceData <- function(s.data, batch, group, cand.DE.genes,
   }else if(is.null(batch)){
     sub.batchs <- 1
     batch <- rep(1, ncol(s.data))
-  }else{
-    stop("Invalid number of batches passed!")
   }
   
-  # # simulate library size  
-  # LL <- simLibSize(sub.batchs=sub.batchs, L=LS, lib.size.params = lib.size.params, 
-  #                  n.batch=n.batch, batch = batch, n.group = n.group, 
-  #                  config.mat = config.mat, ...)
-  
   # select candidate genes
-  if(is.null(cand.DE.genes) & !is.null(group) & length(unique(group))>1){ 
-    X <- group
-    cand.DE.genes <- chooseCandGenes(cpm.data=cpm.data, X=X, const=const,
-                                  lfc.thrld=lfc.thrld, llStat.thrld=llStat.thrld, w=w, ...)
-  }else if(is.null(cand.DE.genes) & (is.null(group) | length(unique(group))==1)){
-    cand.DE.genes <- list(null.genes= rownames(s.data)) 
-  } 
+  if(is.null(cand.DE.genes)){
+    cand.DE.genes = if(!is.null(group) & length(unique(group))>1){ 
+     chooseCandGenes(cpm.data = cpm.data, X = group, const = const, 
+                     lfc.thrld = lfc.thrld, llStat.thrld = llStat.thrld, w = w, 
+                     ...)
+    } else {
+      list(null.genes= rownames(s.data)) 
+    } 
+  }
   list(cand.DE.genes=cand.DE.genes, cpm.data=cpm.data, sub.batchs=sub.batchs)
-  #list(cand.DE.genes=cand.DE.genes, LL=LL, cpm.data=cpm.data, sub.batchs=sub.batchs)
 }
