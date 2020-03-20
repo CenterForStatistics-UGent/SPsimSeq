5fed4a49348a81740e9b84ac2dd42b5c4afa6dfb
shawinke <stijn.hawinkel@ugent.be>
2020-03-20 15:06:30 +0100

    Streamlining density estimation
    Former-commit-id: b0dc468b08b9870b3f06382f1777ac85e47607dc
    Former-commit-id: 10bdbd361bb56f80e8f6fab8ec5a859fca71f6a4
    Former-commit-id: bcebc988593e0e8b8ca1665f0b53b8182a4f6818
    Former-commit-id: 3db7048938b22215bb4952fa28eb8737c09caa08
    Former-commit-id: 01b2ea96a02a85a03b301ed45ff4b6e4b1beb6a4
    Former-commit-id: 57171ad83f44ed4090625f536b99de1bc193228c
    Former-commit-id: 4a0f2f2e4abdfe50f92be75d4b34225447751185
    Former-commit-id: 9c5555c5cdb3bc618021944625dc989389a7ffa5

--- R/geneParmEst.R
+++ R/geneParmEst.R
@@ -1,52 +1,49 @@
-# Gene level param estimates for density estimation
-gene.parm.est <- function(cpm.data.i, batch, group, null.group,
-                          sub.batchs, de.ind, model.zero.prob,
-                          min.val, w,...){
-  batch.est <- lapply(sub.batchs, function(b){ 
-    if(de.ind==0){
+#' Gene level param estimates for density estimation
+#'
+#' @param cpm.data.i full vector of genewise observation
+#' @param batch,group batch and group information 
+#' @param null.group the null group
+#' @param sub.batchs 
+#' @param de.ind a boolean, is the gene to be DE?
+#' @param model.zero.prob a boolean, should zero-density be modelled?
+#' @param min.val minimum value
+#' @param w weight
+#'
+#' @return list of density estimates
+geneParmEst <- function(cpm.data.i, batch, group, null.group, sub.batchs, 
+                          de.ind, model.zero.prob, min.val, w){
+  lapply(sub.batchs, function(b){ 
+    if(!de.ind){
       Y0 <- cpm.data.i[(batch==b & group==null.group)] 
-      Y = if(model.zero.prob & mean(Y0==min.val)>0.25){
-        Y0[Y0>min.val]
-        } else Y0
-      
-    if(sum(Y>min.val)>3){
-        countY   <- obtCount(Y=Y, w=w)
-        parm.est <- fitLLmodel(yy=countY)
-        # if(!is.null(parm.est$betas) & length(countY$S)>=3){
-        #   parm.est
-        # } 
-        # else{NULL}  
-        parm.est
-      } 
-      else{
-        list(S=0, lls=0, uls=0, Ny=0, Y=Y, mu.hat=mean(Y), sig.hat=sd(Y), 
-                w=w, betas=parmEstOut(llm = NULL)$beta.hat.vec, 
-                v = parmEstOut(llm = NULL)$V.hat.mat)
-        }
-    }
-    else{
+      parmEstDensVec(Y0, modelzero.prob, min.val)
+    } else {
       Y0 <- split(cpm.data.i[batch==b], group[batch==b])
       Y <- lapply(Y0, function(y0){
-        if(model.zero.prob & mean(y0==min.val)>0.25) {y0[y0>min.val]}
-        else {y0}
-      }) 
-      lapply(Y, function(y){
-        if(sum(y>min.val)>3){
-          countY   <- obtCount(Y=y, w=w)
-          parm.est <- fitLLmodel(yy=countY)
-          # if(!is.null(parm.est$betas) & length(countY$S)>=3){
-          #   parm.est
-          # } 
-          # else{NULL}  
-          parm.est
-        } 
-        else{
-          list(S=0, lls=0, uls=0, Ny=0, Y=y, mu.hat=mean(y), sig.hat=sd(y), 
-               w=w, betas=parmEstOut(llm = NULL)$beta.hat.vec, 
-               v = parmEstOut(llm = NULL)$V.hat.mat)
-        }
+        parmEstDensVec(y0, modelzero.prob, min.val)
       }) 
     }
   }) 
-  return(batch.est)
+}
+#' Density estimation on a single vector
+#'
+#' @param Y0 the vector of observations
+#' @param model.zero.prob,min.val see geneParmEst()
+#' @param prev.min.val minimum prevalence of minimum values
+#' @param min.count.nonnull minimum count for estimation
+#'
+#' @return density estimates
+parmEstDensVec = function(Y0, model.zero.prob, min.val, prev.min.val = 0.25, 
+                           min.count.nonnull = 3){
+  Y = if(model.zero.prob & mean(Y0==min.val) > prev.min.val){
+  Y0[Y0>min.val]
+  } else Y0
+if(sum(Y > min.val) > min.count.nonnull){
+  countY   <- obtCount(Y = Y, w = w)
+  fitLLmodel(yy = countY)
+} 
+else{
+  list(S=0, lls=0, uls=0, Ny=0, Y=Y, mu.hat=mean(Y), sig.hat=sd(Y), 
+       w=w, betas=parmEstOut(llm = NULL)$beta.hat.vec, 
+       v = parmEstOut(llm = NULL)$V.hat.mat)
+  }
 }
