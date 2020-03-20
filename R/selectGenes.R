dafc6e7a0013c16b858a79f0d6f6ad7f1576d533
shawinke <stijn.hawinkel@ugent.be>
2020-03-20 15:25:07 +0100

    Gene selection
    Former-commit-id: be1cb5cca91abcdb95dd153a52c0618102f15bed
    Former-commit-id: 0a8dcd753a42c83546637a68fc652f775d403e2b
    Former-commit-id: d4a17e04e3a3d5d9ed839dfd01659bc9d1201d35
    Former-commit-id: c49a79206a0f91cac875b736f74f4507bedfe6b9
    Former-commit-id: b341c6365dafb2d96fd0bc676eab1fe05efe9070
    Former-commit-id: 63bf8c024d4828befb148afd760650adb3beb70a
    Former-commit-id: bbbccd68220cf2780035f25efa56f5cb327ec83a
    Former-commit-id: 035ae2dee636105ccb1cb20884d5f92289430d15

--- R/selectGenes.R
+++ R/selectGenes.R
@@ -2,26 +2,22 @@
 selectGenes <- function(pDE, group, n.genes, null.genes0, nonnull.genes0,
                            group.config, ...){
   if(pDE>0 & !is.null(group) & length(group.config)>1){
-    if((1-pDE)*n.genes <= length(null.genes0)){
-      null.genes     <- sample(null.genes0, (1-pDE)*(n.genes), replace = FALSE)
-    }
-    else{
-      null.genes     <- sample(null.genes0, (1-pDE)*(n.genes), replace = TRUE)
+    null.genes = if((1-pDE)*n.genes <= length(null.genes0)){
+      sample(null.genes0, (1-pDE)*(n.genes), replace = FALSE)
+    } else{
+      sample(null.genes0, (1-pDE)*(n.genes), replace = TRUE)
     }
     
-    if(length(nonnull.genes0)>0){
+    nonnull.genes = if(length(nonnull.genes0)>0){
       if(pDE*n.genes <= length(nonnull.genes0)){
-        nonnull.genes  <- sample(nonnull.genes0, pDE*n.genes, replace = FALSE)
+          sample(nonnull.genes0, pDE*n.genes, replace = FALSE)
       }
       else {
-        nonnull.genes  <- sample(nonnull.genes0, pDE*n.genes, replace = TRUE)
+          sample(nonnull.genes0, pDE*n.genes, replace = TRUE)
       }
-    }
-    else{ 
-      nonnull.genes <- NULL
-    }
+    } else {NULL}
     sel.genes <- c(nonnull.genes, null.genes)
-  }  else{
+  } else{
       sel.genes <- sample(null.genes0, n.genes, 
                           replace = n.genes <= length(null.genes0))
   } 
