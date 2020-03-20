821a9e03ce071ad4e9b9a8bd9f3279c02cfc36ac
shawinke <stijn.hawinkel@ugent.be>
2020-03-20 10:46:21 +0100

    Do away with seq_len(length(
    Former-commit-id: 3da682759fbd98c51dde75fabb457406c1e4953e
    Former-commit-id: 369c0188968e064164e3e08351a15276d5e1e668

--- R/parmEstOut.R
+++ R/parmEstOut.R
@@ -11,7 +11,7 @@ parmEstOut <- function(llm){
     betas= numeric(5)
     v=matrix(0, 5, 5)
   }
-  beta.hat.vec[seq_len(length(betas))] <- betas
+  beta.hat.vec[seq_along(betas)] <- betas
   V.hat.mat[seq_len(nrow(v)), seq_len(ncol(v))] <- v
   
   list(beta.hat.vec=beta.hat.vec, V.hat.mat=V.hat.mat)
