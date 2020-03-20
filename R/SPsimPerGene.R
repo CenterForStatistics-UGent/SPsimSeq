4402ea9bde482f75c2d79539ac8ca5128452c3ee
shawinke <stijn.hawinkel@ugent.be>
2020-03-20 15:41:41 +0100

    Streamlining gene selection
    Former-commit-id: 8634e320d36d064ee5d395cd0b7315d7a3871f17
    Former-commit-id: 8622cf322360a986a4c87dabaa04271872958a39

--- R/SPsimPerGene.R
+++ R/SPsimPerGene.R
@@ -1,20 +1,37 @@
-# A function that generates the simulated data for each gene
+#' A function that generates the simulated data for each gene
+#'
+#' @param cpm.data 
+#' @param est.list.ii 
+#' @param DE.ind.ii 
+#' @param n.batch 
+#' @param n.group 
+#' @param batch 
+#' @param group 
+#' @param config.mat 
+#' @param sel.genes.ii 
+#' @param log.CPM.transform 
+#' @param const 
+#' @param min.val 
+#' @param null.group 
+#' @param LL 
+#' @param copulas.batch 
+#' @param tot.samples 
+#' @param model.zero.prob 
+#' @param fracZero.logit.list 
+#'
+#' @return
 SPsimPerGene <- function(cpm.data, est.list.ii, DE.ind.ii, n.batch, n.group, batch, group, config.mat, 
                          sel.genes.ii, log.CPM.transform, const, min.val, null.group, LL,
                          copulas.batch, tot.samples, model.zero.prob, fracZero.logit.list){
-  if(!is.null(est.list.ii)){
     # get batch specific parameters
     par.sample <- obtParSample(est.list.i = est.list.ii, DE.ind.ii = DE.ind.ii, 
                                n.batch = n.batch, group = group)
-    
     # estimate carrier density (g0)
-    g0 <- estCarrierDens(est.list.i = est.list.ii, par.sample=par.sample,
+    g0 <- estCarrierDens(est.list.i = est.list.ii, par.sample = par.sample,
                          DE.ind.ii = DE.ind.ii,  group = group)
-    
     # estimate the density g1(y)
-    g1 <- estSPDens(est.list.i = est.list.ii, par.sample=par.sample,
-                    DE.ind.ii = DE.ind.ii,  group = group, g0 = g0)
-    
+    g1 <- estSPDens(est.list.i = est.list.ii, par.sample = par.sample,
+                    DE.ind.ii = DE.ind.ii, group = group, g0 = g0)
     # simulate data 
     Y.star <- sampleDatSPDens(cpm.data=cpm.data, sel.genes.i=sel.genes.ii, par.sample=par.sample, 
                               DE.ind.ii=DE.ind.ii, null.group=null.group, LL=LL,
@@ -23,14 +40,7 @@ SPsimPerGene <- function(cpm.data, est.list.ii, DE.ind.ii, n.batch, n.group, bat
                               min.val=min.val, n.group=n.group, n.batch=n.batch,
                               config.mat=config.mat, model.zero.prob=model.zero.prob, 
                               fracZero.logit.list=fracZero.logit.list)
-    if(DE.ind.ii==1){
-      names(g1) <- names(g0) <- names(par.sample) <- paste0("group_", unique(group))
-    }else{
-      names(g1) <- names(g0) <- names(par.sample) <- paste0("group_", null.group)
-    }
+    names(g1) <- names(g0) <- names(par.sample) <- 
+      paste0("group_", if(DE.ind.ii) unique(group) else null.group)
      return(Y.star)
-  }
-  else{ #print(i) 
-   return(numeric(tot.samples))
-  }
 }
