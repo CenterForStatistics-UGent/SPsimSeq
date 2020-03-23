6ce3762a1ce52e6bb15cdbc620557396cdb7c80e
shawinke <stijn.hawinkel@ugent.be>
2020-03-23 10:18:19 +0100

    Now checking the parameter estimation
    Former-commit-id: 0c32df50a26206da1d1aa5b6883adb4403a3eb6e
    Former-commit-id: 335b1fff4d1d49db6618286ffc82f9b3c432264e

--- R/configExperiment.R
+++ R/configExperiment.R
@@ -1,64 +1,46 @@
-# Configure experiment
-# 
-# @param batch.config a numerical vector for the marginal fraction of samples in each batch. 
-# The number of batches to be simulated is equal to the size of the vector.
-# All values must sum to 1.
-# @param group.config a numerical vector for the marginal fraction of samples in each group.
-# The number of groups to be simulated is equal to the size of the vector. All values must sum to 1.
-# @param tot.samples total number of samples to be simulated.
-# 
-# @return a list object contating the number of groups and batches to be simukated, 
-# and the experiment configurartion 
-# @examples 
-# \donttest{
-# #---- a design with a total of 10 samples/cells from 1 batch and 1 group
-# expriment.config(batch.config=1, group.config=1, tot.samples=10)
-# 
-# #---- a design with a total of 20 samples/cells from 1 group and 2 batchs with 
-# # batch 1 has 15 samples/cells and batch 2 has 5
-# expriment.config(batch.config=c(15/20, 5/20), group.config=1, tot.samples=20)
-# 
-# #---- a design with a total of 20 samples/cells from 1 batch and 2 groups with 
-# # group 1 has 10 samples/cells and batch 2 has 10
-# expriment.config(batch.config=1, group.config=c(0.5, 0.5), tot.samples=20)
-# 
-# #---- a design with a total of 30 samples/cells from 2 groups with group 1 has 15 samples 
-# # and group 2 has 15, and  three batchs with batch 1,2, and 3 have 5, 10, and 15 samples/cells, 
-# # respectively.
-# expriment.config(batch.config=c(5/30, 10/30, 15/30), group.config=c(0.5, 0.5), tot.samples=30)
-# }
-configExperiment <- function(batch.config, group.config, tot.samples = 150){
-  
+#' Configure experiment
+#' 
+#' @param batch.config a numerical vector for the marginal fraction of samples in each batch. 
+#' The number of batches to be simulated is equal to the size of the vector.
+#' All values must sum to 1.
+#' @param group.config a numerical vector for the marginal fraction of samples in each group.
+#' The number of groups to be simulated is equal to the size of the vector. All values must sum to 1.
+#' @param tot.samples total number of samples to be simulated.
+#'
+#' @return a list object contating the number of groups and batches to be simukated, 
+#' and the experiment configurartion 
+#' @examples 
+#' \donttest{
+#' #---- a design with a total of 10 samples/cells from 1 batch and 1 group
+#' expriment.config(batch.config=1, group.config=1, tot.samples=10)
+#' 
+#' #---- a design with a total of 20 samples/cells from 1 group and 2 batchs with 
+#' # batch 1 has 15 samples/cells and batch 2 has 5
+#' expriment.config(batch.config=c(15/20, 5/20), group.config=1, tot.samples=20)
+#' 
+#' #---- a design with a total of 20 samples/cells from 1 batch and 2 groups with 
+#' # group 1 has 10 samples/cells and batch 2 has 10
+#' expriment.config(batch.config=1, group.config=c(0.5, 0.5), tot.samples=20)
+#' 
+#' #---- a design with a total of 30 samples/cells from 2 groups with group 1 has 15 samples 
+#' # and group 2 has 15, and  three batchs with batch 1,2, and 3 have 5, 10, and 15 samples/cells, 
+#' # respectively.
+#' expriment.config(batch.config=c(5/30, 10/30, 15/30), group.config=c(0.5, 0.5), tot.samples=30)
+#' }
+configExperiment <- function(batch.config, group.config, tot.samples){
+  #Assign names
   if(is.null(names(batch.config))){
     names(batch.config) <- paste0("Batch_",seq_along(batch.config))
   }
   if(is.null(names(group.config))){
     names(group.config) <- paste0("Group_",seq_along(group.config))
   }
-  
+  #Configure the experiment
   exprmt.config <- round(tcrossprod(batch.config, group.config)*tot.samples)
   rownames(exprmt.config) <- names(batch.config)
   colnames(exprmt.config) <- names(group.config)
-  
+  #Total per group and batch
   n.batch <- rowSums(exprmt.config)
   n.group <- colSums(exprmt.config)
-  
-  # design element
-  n.batch <- exprmt.design$n.batch
-  n.group <- exprmt.design$n.group
-  config.mat <- exprmt.design$exprmt.config
-  
-  # subset batches
-  if(!is.null(batch)){ 
-    if(length(n.batch) < length(unique(batch))){
-      sub.batchs <- sort(sample(length(unique(batch)), length(n.batch))) 
-    }else if(length(n.batch) == length(unique(batch))){
-      sub.batchs <- seq_along(n.batch)
-    }
-  }else if(is.null(batch)){
-    sub.batchs <- 1
-    batch <- rep(1, ncol(s.data))
-  }
-  
-  list(exprmt.config=exprmt.config, n.batch=n.batch, n.group=n.group)
+  list(exprmt.config = exprmt.config, n.batch = n.batch, n.group = n.group)
 }
--- R/fitLLmodel.R
+++ R/fitLLmodel.R
@@ -1,25 +1,24 @@
 #' Fit log linear model for each gene
 #' 
 #' @param yy a list object contating a result from obtCount() function for a single gene 
-#' @param  ... further arguments passed to or from other methods.
+#' @param mu.hat,sig.hat Carrier density estimators
+#' @param n number of observations
+#' @param prior.count the prior count
 #' 
 #' @return a list object containing the fitted log linear model and carrier density
 #' @importFrom stats pnorm
-fitLLmodel <- function(yy, ...){
-  #Carrier density
-  g0 = suppressWarnings(pnorm(yy$uls, yy$mu.hat, yy$sig.hat) - 
-    pnorm(yy$lls, yy$mu.hat, yy$sig.hat))
-  ofs = log(g0*sum(yy$Ny)+1)
-  llm = NULL
-  degree = 4
+fitLLmodel <- function(yy, mu.hat, sig.hat, n, prior.count){
+  #Evaluate carrier density
+  g0 = diff(pnorm(yy$breaks, mean = mu.hat, sd = sig.hat))
+  ofs = log(g0*n + prior.count)
+  llm = NULL; degree = 4
   while(is.null(llm) && (degree >= 1)){
-  llm <- tryCatch(fitPoisGlm(yy$Ny, yy$S, degree, offset = ofs), 
+  llm <- tryCatch(fitPoisGlm(yy$counts, yy$mids, degree, offset = ofs), 
                   error=function(e){}, warning=function(w){}) 
   if(!is.null(llm) && llm$rank != ncol(llm$R)){
     llm = NULL
   }
   degree = degree - 1
   }
-  est.parms <- extractGlmParams(llm)
-  return(c(yy, est.parms, list(g0 = g0)))
+  return(c(yy, list(g0 = g0, coef = llm$coef)))
 }
