823427701911f27d2e37e841764a560454312c36
shawinke <stijn.hawinkel@ugent.be>
2020-03-24 10:31:49 +0100

    Proper sorting in experiment configuration
    Former-commit-id: b985b0f623441a26385ac5c98f913893bbe856c0
    Former-commit-id: 9ca81e0f971fd8c9bd28196084840d739ead77a3
    Former-commit-id: 384f6ac5a91e145096c30bcdc1a5541e59691048
    Former-commit-id: dea6a694e42fb711fb13a43d13acc497f96a4d86
    Former-commit-id: a21c9b456ab756369931b6886b24ba75b4294c4b

--- R/configExperiment.R
+++ R/configExperiment.R
@@ -6,6 +6,7 @@
 #' @param group.config a numerical vector for the marginal fraction of samples in each group.
 #' The number of groups to be simulated is equal to the size of the vector. All values must sum to 1.
 #' @param tot.samples total number of samples to be simulated.
+#' @param batch,group batch and grouping vectors
 #'
 #' @return a list object contating the number of groups and batches to be simukated, 
 #' and the experiment configurartion 
@@ -28,7 +29,7 @@
 #' # respectively.
 #' expriment.config(batch.config=c(5/30, 10/30, 15/30), group.config=c(0.5, 0.5), tot.samples=30)
 #' }
-configExperiment <- function(batch.config, group.config, tot.samples, batch){
+configExperiment <- function(batch.config, group.config, tot.samples, batch, group){
   #Assign names
   if(is.null(names(batch.config))){
     names(batch.config) <- paste0("Batch_", seq_along(batch.config))
@@ -36,6 +37,9 @@ configExperiment <- function(batch.config, group.config, tot.samples, batch){
   if(is.null(names(group.config))){
     names(group.config) <- paste0("Group_", seq_along(group.config))
   }
+  #Sort, such that largest groups and largest batches match with simulation
+  batch.config = sort(batch.config, decreasing = TRUE)
+  group.config = sort(group.config, decreasing = TRUE)
   #Configure the experiment
   exprmt.config <- round(tcrossprod(batch.config, group.config)*tot.samples)
   rownames(exprmt.config) <- names(batch.config)
@@ -43,9 +47,15 @@ configExperiment <- function(batch.config, group.config, tot.samples, batch){
   #Total per group and batch
   n.batch <- rowSums(exprmt.config)
   n.group <- colSums(exprmt.config)
-  #Sample batches if needed
-  if(nrow(exprt.config)>1){
-   sub.batchs 
+  #Select most abundant groups and batches to sample from
+  if(nrow(exprt.config) < length(unqiue(batch))){
+    batchKeep = names(sort(table(batch), decreasing = TRUE))[seq_len(nrow(exprmt.config))]
+    sub.batchs = rep(rep(batchKeep, ncol(exprmt.config)), times = c(exprmt.config)) 
   }
-  list(exprmt.config = exprmt.config, n.batch = n.batch, n.group = n.group)
+  if(ncol(exprt.config) < length(unqiue(group))){
+    groupKeep = names(sort(table(group), decreasing = TRUE))[seq_len(ncol(exprmt.config))]
+    sub.groups = rep(groupKeep, times = n.group) 
+  }
+  list(exprmt.config = exprmt.config, sub.batchs = sub.batchs, 
+       sub.groups = sub.groups)
 }
