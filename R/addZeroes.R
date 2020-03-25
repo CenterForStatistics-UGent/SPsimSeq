7edc064ac9f13ce45a78aca916700ac49f2ed9f8
shawinke <stijn.hawinkel@ugent.be>
2020-03-25 11:30:45 +0100

    New functions
    Former-commit-id: 8290a33efdd1853a501f4cc9926d52df9bf9c12b
    Former-commit-id: 02a413ea3e61ec0143e474ce927ffa7fc58abf1a
    Former-commit-id: a79f162f186f9b9fbe851f708c5b0c2d9c6d2adc
    Former-commit-id: 52d89d830493494790e647ab09dc17320f0f6336
    Former-commit-id: b29162b67cc2a59989a7956f9a8e04c662f4f872

---
+++ R/addZeroes.R
@@ -0,0 +1,17 @@
+addZeroes = function(Y, fracZero.logit.list){
+  if(model.zero.prob & mean(Y0==min.val)>0.25){
+    lLL_b <- log(LL.b)
+    pred.pz  <- try(predict(fracZero.logit.list[[bb]], type="response",
+                            newdata=data.frame(x1=mean(Y0), x2=lLL_b)), 
+                    silent = TRUE)
+    if(!is(pred.pz,"try-error")){
+      #set.seed(sim.seed+2)
+      drop.mlt <- sapply(pred.pz, function(p){ 
+        rbinom(1, 1, p) 
+      })
+    }else{
+      drop.mlt <- 0
+    } 
+    y.star.b <- y.star.b*(1-drop.mlt)
+  }
+}
