5eec11e671fbca66f8da7355bdc4f8c82ec44c1f
shawinke <stijn.hawinkel@ugent.be>
2020-03-25 13:09:43 +0100

    zero generation almost done
    Former-commit-id: bf44c1bae5f9cca22024852d8a7c8fd7c8e3e84c
    Former-commit-id: d3db190c05f7eb9733b3194b16dc488f07edffba

--- R/fracZeroLogitModel.R
+++ R/fracZeroLogitModel.R
@@ -1,14 +1,22 @@
 #' Extract data and iterate over batches to estimate zero probability models
 #'
-#' @param s.data 
-#' @param batch 
+#' @param s.data,cpm.data raw and transformed data 
+#' @param batch the batch vector
+#' @param n.mean.class see zeroProbModel
+#' @param minFracZeroes minimum fraction of zeroes before zero-inflation is applied
 #'
 #' @return a list of binomial regression parameters
-fracZeroLogitModel <- function(s.data, batch, cpm.data){
+fracZeroLogitModel <- function(s.data, batch, cpm.data, n.mean.class, 
+                               minFracZeroes){
   LS = colSums(s.data)
   zeroMat = s.data == 0
-  tapply(colnames(s.data), batch, function(coln){
+  zeroModels = tapply(colnames(s.data), batch, function(coln){
     zeroProbModel(cpm.data = cpm.data[, coln], logL = log(LS[coln]), 
-                  zeroMat[, coln])
+                  zeroMat[, coln],  n.mean.class = n.mean.class, 
+                  minFracZeroes = minFracZeroes)
   })
+  zeroFracs = vapply(colnames(s.data), function(gene){
+    tapply(zeromat[gene, ], batch, mean)
+  }, FUN.VALUE = numeric(length(unique(batch))))
+  list(zeroModels = zeroModels, exceedIds = exceedIds)
 }
--- R/zeroProbModel.R
+++ R/zeroProbModel.R
@@ -3,24 +3,13 @@
 #' @param logL log library size of the source data
 #' @param n.mean.class a fraction of the number of genes
 #' for the number of groups to be created for the mean log CPM of genes
-#' @param subset.data a logical value to subset columns(samples) if the size of the data is too big
-#' for space and computation time saving
-#' @param n.samples an integer indicating the number of samples/cells to sample (if subset.data=TRUE)
-#' @param const a small constant (>0) to be added to the CPM before log transformation, to avoid  log(0)
 #'
 #' @return The coefficients of the estimated logistic regression
 #' @importFrom stats glm.fit, binomial
 #' @importFrom Hmisc cut2
-zeroProbModel <- function(cpm.data, logL, zeroMat, n.mean.class = 0.2, subset.data = FALSE, 
-                          n.samples = 400){
-  #Subset data if needed
-  if(subset.data & (ncol(cpm.data) > n.samples)){
-    sel.cols <- sample(ncol(cpm.data), n.samples)
-    cpm.data <- cpm.data[, sel.cols]
-    logL <- logL[sel.cols]
-    zeroMat = zeroMat[, sel.cols]
-  } 
-  # Fit a logistic model for the probability of zeros
+zeroProbModel <- function(cpm.data, logL, zeroMat, n.mean.class){
+  # Fit a logistic model for the probability of zeros, if there are enough of them
+  if(mean(zeroMat) > minFracZeroes){
   mean.log.cpm <- rowMeans(cpm.data) #Means
   n.mean.class <- round(n.mean.class*nrow(cpm.data))
   mid.val <- as.numeric(as.character(cut2(mean.log.cpm, g = n.mean.class, 
@@ -28,6 +17,10 @@ zeroProbModel <- function(cpm.data, logL, zeroMat, n.mean.class = 0.2, subset.da
   x1 <- rep(mid.val, times = length(log.L))
   x2 <- rep(log.L, each = length(mid.val))
   desMat = model.matrix(~x1*x2)
-  fracZero.logit <- suppressWarnings(glm.fit(y = c(zeroMat), x = desMat, family = binomial()))
-  fracZero.logit$coef
+  fracZero.logit <- suppressWarnings(glm.fit(y = c(zeroMat), x = desMat, 
+                                             family = binomial()))
+  return(fracZero.logit$coef)
+  } else{
+    return(NULL)
+  }
 }
