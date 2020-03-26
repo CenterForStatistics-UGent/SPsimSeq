3b5c009face06c111e017922e83675451bcd746f
shawinke <stijn.hawinkel@ugent.be>
2020-03-26 12:54:39 +0100

    Address BiocCheck concerns
    Former-commit-id: 41be08b04db46c39b97069fd1b33826e0d7b3dcd
    Former-commit-id: 0e267131a239fac97872c473446071d1b9a7e4ef
    Former-commit-id: 84db03376ec589b3b9437c6146cd7c3323aa37c3
    Former-commit-id: bb4ba147db8987a1a5fb951f9dac1f63314bf0a1
    Former-commit-id: 285f7ee1783ae6f070daa5711b4cd04720ce937a

--- R/genLibSizes.R
+++ R/genLibSizes.R
@@ -4,8 +4,8 @@
 #' @param exprmt.design 
 #'
 #' @return The generated libray sizes per batch and group
+#' @importFrom stats rlnorm
 genLibSizes = function(fit.ln, exprmt.design){
-  Seq = seq_along(n.group)
   n.batch = rowSums(exprmt.design$exprmt.config)
   lapply(unique(exprmt.design$sub.batchs), function(b){
     rlnorm(n.batch[b], fit.ln[[b]][["meanlog"]], fit.ln[[b]][["sdlog"]])
