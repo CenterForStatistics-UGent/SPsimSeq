1ae24f641d5db658464070ad42bb1f2d0328fe6b
shawinke <stijn.hawinkel@ugent.be>
2020-03-26 11:25:08 +0100

    Functions running, docu is next
    Former-commit-id: 56db5f605f0da442b29557c9bbfbd62cbe1d5408
    Former-commit-id: 88c729a3224295d519816d357f22aba88a05ec73

--- R/extractMat.R
+++ R/extractMat.R
@@ -1,5 +1,5 @@
 #' A function with S4 dispatching to extract the count matrix
-#' @param Y a matrix, data frame or SingleCellExperiment
+#' @param Y a matrix, data frame, phyloseq object or SingleCellExperiment
 #' @param ... additional arguments, currently ignored
 #' @return A data matrix with samples in the columns and genes in the rows
 #' @rdname extractMat
@@ -17,4 +17,12 @@ setMethod("extractMat", "matrix", function(Y, ...){
 #' @rdname extractMat
 setMethod("extractMat", "data.frame", function(Y, ...){
   as.matrix(t(Y))
+})
+#' @rdname extractMat
+#' @importFrom phyloseq t taxa_are_rows otu_table
+setMethod("extractMat", "phyloseq", function(Y, ...){
+  if(!taxa_are_rows(Y)){
+    Y = t(Y)
+  }
+  as(otu_table(Y), "matrix")
 })
