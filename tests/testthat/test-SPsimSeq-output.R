232f50c044f9b1a0c0d77bcb1d717a73db0cb03b
shawinke <stijn.hawinkel@ugent.be>
2020-03-27 16:00:19 +0100

    Change names of test files
    Former-commit-id: 9a2ffb59a323975bfb019a54eb152fde291e654b
    Former-commit-id: ae4f8baf7779d4922e109794bc9004dfeee3219f
    Former-commit-id: bb663e64cca05fbd2aef34415e39462b2d911e7f
    Former-commit-id: 77bae601c0f3bad84795120b2ebb14b9b9106ad4
    Former-commit-id: 1b6397079ae26cbba5f7588d0cc6a3bc090440ae
    Former-commit-id: a6f141560c30dea382ae23adac444cd3335441f5

---
+++ tests/testthat/test-SPsimSeq-output.R
@@ -0,0 +1,16 @@
+context("Output of the SPsimSeq function")
+data("zhang.data.sub")
+zhang.counts <- zhang.data.sub$counts
+MYCN.status  <- zhang.data.sub$MYCN.status
+test_that("SPsimSeq returns correct output types", {
+  expect_type(SPsimSeq(n.sim = 1, s.data = zhang.counts,
+                          group = MYCN.status, n.genes = 2000, batch.config = 1,
+                          group.config = c(0.5, 0.5), tot.samples = 20,
+                          pDE = 0.1, lfc.thrld = 50, result.format = "list"),
+              "list")
+  expect_s4_class(SPsimSeq(n.sim = 1, s.data = zhang.counts,
+                       group = MYCN.status, n.genes = 2000, batch.config = 1,
+                       group.config = c(0.5, 0.5), tot.samples = 20,
+                       pDE = 0.1, lfc.thrld = 50, result.format = "SCE"),
+              "SingleCellExperiment")
+})
