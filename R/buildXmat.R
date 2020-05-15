309d365275415f52f454375876459f7d3eaffb2f
shawinke <stijn.hawinkel@ugent.be>
2020-05-15 12:49:24 +0200

    No more : loops

--- R/buildXmat.R
+++ R/buildXmat.R
@@ -1,14 +1,14 @@
-#'An auxialiary function to quickly construct the polyomial matrix, 
+#'An auxialiary function to quickly construct the polyomial matrix,
 #'using Horner's rule
 #'
-#'@param x The base 
+#'@param x The base
 #'@param nc the number of columns
 #'
 #'@return A matrix with increasing powers of x in the columns
 buildXmat = function(x, nc){
   x.mat <- matrix(1, nrow = length(x), ncol = nc)
-  for(k in 2:nc){
-    x.mat[,k:nc] <- x.mat[,k:nc]*x
+  for(k in seq_len(nc)[-1]){
+    x.mat[,seq(k,nc)] <- x.mat[,seq(k,nc)]*x
   }
   x.mat
 }
